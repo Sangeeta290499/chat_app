a933f458df796f09d7339e12101b4506f69c0891
Sangeeta Varun <sangeetavarun7@gmail.com>
2025-01-25 23:39:19 +0530

    Create user.controller.js

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,9 @@
+//here we will write registration things
+
+export const register = async (req, res) => {
+    try{
+        const {username, password, email} = req.body;
+    } catch(error){
+
+    }
+}
