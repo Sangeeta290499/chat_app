40e00a0afc645afc2fe75037e3568496c03f43b8
Sangeeta Varun <sangeetavarun7@gmail.com>
2025-01-28 23:10:23 +0530

    updated user route file

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getProfile, login, logout, register } from "../controllers/user.controller.js";
+import { editProfile, followOrUnfollow, getProfile, getSuggestedUsers, login, logout, register } from "../controllers/user.controller.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
@@ -7,4 +7,8 @@ router.route('/register').post(register);
 router.route('/login').get(login);
 router.route('/logout').get(logout);
 router.route('/:id/profile').get(isAuthenticated, getProfile);
-router.route('/register').post(register);
+router.route('/register').post(isAuthenticated,upload.single('profilePicture'), editProfile);
+router.route('/suggested').get(isAuthenticated,getSuggestedUsers);
+router.route('/followorunfollow/:id').post(isAuthenticated,followOrUnfollow);
+
+export default router;
